0f0d8dcc0c163750e2610911653b6838121b9f24
Lucas <lucas.ghaspar@gmail.com>
2022-10-15 17:15:59 -0300

    insert ant design basic use

--- pages/lista/index.js
+++ pages/lista/index.js
@@ -1,20 +1,80 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Button, DatePicker, Space, version } from "antd";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import React, { useEffect, useState } from "react";
 import Link from 'next/link';
 
-export default function Lista({animesList}) {
+const { Header, Sider, Content } = Layout;
 
+export default function Lista({animesList}) {
+    const [collapsed, setCollapsed] = useState(false);
     return (
       <>
-        <h1>Catalogo dos animes! </h1>
+        <Layout className="layout">
+          <Sider trigger={null} collapsible collapsed={collapsed}>
+            <div className="logo" />
+            <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                items={[
+                  {
+                    key: '1',
+                    icon: <UserOutlined />,
+                    label: 'nav 1',
+                  },
+                  {
+                    key: '2',
+                    icon: <VideoCameraOutlined />,
+                    label: 'nav 2',
+                  },
+                  {
+                    key: '3',
+                    icon: <UploadOutlined />,
+                    label: 'nav 3',
+                  },
+                ]}
+            />
+          </Sider>
+          <Layout className="site-layout">
+            <Header
+                className="site-layout-background"
+                style={{
+                  padding: 0,
+                }}
+            >
+              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: () => setCollapsed(!collapsed),
+              })}
+            </Header>
+            <Content
+                className="site-layout-background"
+                style={{
+                  margin: '24px 16px',
+                  padding: 24,
+                }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+        {/* <h1>Catalogo dos animes! </h1>
+        <Button type="primary">Button</Button>
         {
           animesList.map((obj) => (
             <article key={obj.id}>
               <p></p>
             </article>
           ))
-        }
+        } */}
 
       </>
     )
@@ -25,8 +85,8 @@ export async function getServerSideProps() {
 
   const res = await fetch("https://kitsu.io/api/edge/anime")
   const animes = await res.json()
-  console.log(animes.data);
-  
+  console.log(animes.data[1].attributes.titles.en);
+
   return { props: { 
     animesList: animes.data
    } 

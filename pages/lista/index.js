38e6d16c9241e0315e8da2c0e85c5a51c24169b6
Lucas <lucas.ghaspar@gmail.com>
2022-10-15 19:15:55 -0300

    insert API return to front + antdesign

--- pages/lista/index.js
+++ pages/lista/index.js
@@ -7,11 +7,17 @@ import {
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import { Col, Layout, Menu, Row } from 'antd';
 import React, { useEffect, useState } from "react";
 import Link from 'next/link';
+import { CustomPlaceholder } from 'react-placeholder-image';
 
 const { Header, Sider, Content } = Layout;
+
+const col = {
+  padding: '2rem 1rem',
+  maxWidth: '400px'
+};
 
 export default function Lista({animesList}) {
     const [collapsed, setCollapsed] = useState(false);
@@ -19,29 +25,29 @@ export default function Lista({animesList}) {
       <>
         <Layout className="layout">
           <Sider trigger={null} collapsible collapsed={collapsed}>
-            <div className="logo" />
-            <Menu
-                theme="dark"
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                items={[
-                  {
-                    key: '1',
-                    icon: <UserOutlined />,
-                    label: 'nav 1',
-                  },
-                  {
-                    key: '2',
-                    icon: <VideoCameraOutlined />,
-                    label: 'nav 2',
-                  },
-                  {
-                    key: '3',
-                    icon: <UploadOutlined />,
-                    label: 'nav 3',
-                  },
-                ]}
-            />
+            <div className="title">Categorias dos Animes</div>
+              <Menu
+                  theme="dark"
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  items={[
+                    {
+                      key: '1',
+                      icon: <UserOutlined />,
+                      label: 'nav 1',
+                    },
+                    {
+                      key: '2',
+                      icon: <VideoCameraOutlined />,
+                      label: 'nav 2',
+                    },
+                    {
+                      key: '3',
+                      icon: <UploadOutlined />,
+                      label: 'nav 3',
+                    },
+                  ]}
+              />
           </Sider>
           <Layout className="site-layout">
             <Header
@@ -57,24 +63,33 @@ export default function Lista({animesList}) {
             </Header>
             <Content
                 className="site-layout-background"
-                style={{
-                  margin: '24px 16px',
-                  padding: 24,
-                }}
+                id="show-all-animes"
+                col=""
             >
-              Content
+              <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                {
+                  animesList.map((obj) => (
+                    <Col style={col}>
+                      <article key={obj.id}>
+                        {
+                          obj.attributes.coverImage?.small 
+                          ?
+                          <Image src={obj.attributes.coverImage?.small}
+                            width="384"
+                            height="91"
+                            alt="teste" />
+                          :
+                          <CustomPlaceholder width={384} height={91} backgroundColor="#123456" textColor="#ffffff" text="Imagem indisponivel =("/>
+                        }
+                        <h2>{obj.attributes.canonicalTitle}</h2>
+                      </article>
+                    </Col>
+                  ))
+                }
+              </Row>
             </Content>
           </Layout>
         </Layout>
-        {/* <h1>Catalogo dos animes! </h1>
-        <Button type="primary">Button</Button>
-        {
-          animesList.map((obj) => (
-            <article key={obj.id}>
-              <p></p>
-            </article>
-          ))
-        } */}
 
       </>
     )
@@ -85,7 +100,7 @@ export async function getServerSideProps() {
 
   const res = await fetch("https://kitsu.io/api/edge/anime")
   const animes = await res.json()
-  console.log(animes.data[1].attributes.titles.en);
+  console.log(animes.data[4].attributes);
 
   return { props: { 
     animesList: animes.data

6ce09d5f904766fb4a0cadd251b87eab46ffed11
Lucas <lucas.ghaspar@gmail.com>
2022-10-16 02:20:05 -0300

    add card link

--- pages/lista/index.js
+++ pages/lista/index.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { CustomPlaceholder } from 'react-placeholder-image';
 import MainHeader from "../../components/MainHeader";
 import Meta from 'antd/lib/card/Meta';
+import Link from 'next/link';
 
 const { Content } = Layout;
 
@@ -15,28 +16,30 @@ export default function Lista({animesList}) {
             <MainHeader />
             <Content
                 className="site-layout-background"
-                id="show-all-animes"
             >
-              <Row gutter={[12,12]} justify="center">
+              <Row className="content-cards bg-secondary" gutter={[12,12]} justify="center">
                 {
                   animesList.map((obj) => (
                     <Col className="gutter-row" align="middle" xs={24} md={12} lg={4} key={obj.id}>
-                      <Card
-                        hoverable
-                        className="anime-post text-white"
-                        cover={
-                          obj.attributes.posterImage?.small 
-                          ?
-                          <Image src={obj.attributes.posterImage?.small}
-                            width="284"
-                            height="402"
-                            alt="teste" />
-                          :
-                          <CustomPlaceholder width={284} height={402} backgroundColor="#123456" textColor="#ffffff" text="Imagem indisponivel =("/>
-                        }
-                        >
-                        <Meta title={obj.attributes.canonicalTitle} />
-                      </Card>
+                      <Link href={"/lista/"+obj.attributes.slug}>
+                        <Card
+                          hoverable
+                          className="anime-post text-white"
+                          cover={
+                            obj.attributes.posterImage?.small 
+                            ?
+                            <Image src={obj.attributes.posterImage?.small}
+                              width="284"
+                              height="402"
+                              alt="teste" />
+                            :
+                            <CustomPlaceholder width={284} height={402} backgroundColor="#123456" textColor="#ffffff" text="Imagem indisponivel =("/>
+                          }
+                          >
+                          <Meta title={obj.attributes.canonicalTitle} />
+                        </Card>
+                      </Link>
+                      
                     </Col>
                   ))
                 }
